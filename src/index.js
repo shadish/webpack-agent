1adfa1384adbb6b368d5722974e676d74f631544
shadish <shadtisch@gmail.com>
2018-06-18 17:18:05 -0600

    move from react-scripts to webpack with dev server

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const Index = () => {
+	return <div>Hello world!</div>;
+}
+
+ReactDOM.render(<Index />, document.getElementById("index"));
