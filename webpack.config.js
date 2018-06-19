ddfac03ac0a0f283d3f7c05dfb5ed068a982e988
shadish <shadtisch@gmail.com>
2018-06-18 19:25:30 -0600

    setup src folder with placeholder assets

--- webpack.config.js
+++ webpack.config.js
@@ -15,21 +15,8 @@ module.exports = {
         }
       },
       {
-        test: /\.css$/,
-        use: [{
-            loader: "style-loader"
-          },
-          {
-            loader: "css-loader",
-            options: {
-              modules: true,
-              importLoaders: 1,
-              localIdentName: "[name]_[local]_[hash:base64]",
-              sourceMap: true,
-              minimize: true
-            }
-          }
-        ]
+        test: /\.less$/,
+        loader: "less-loader"
       }
     ]
   },
