79ab2f78fc251b9e6b06b079c0e95349661eb6d7
shadish <shadtisch@gmail.com>
2018-06-18 19:07:37 -0600

    add style to react component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import style from "./index.css";
 
 const Index = () => {
-	return <div>Hello world!</div>;
+	return <div className={"style.helloLabel"}>Hello world!</div>;
 }
 
 ReactDOM.render(<Index />, document.getElementById("index"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,13 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
-const htmlPlugin = new HtmlWebPackPlugin({
-	template: './src/index.html',
-	filename: './index.html'
+const htmlWebpackPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
 });
 
 module.exports = {
   module: {
-    rules: [
-      {
+    rules: [{
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
@@ -16,10 +15,23 @@ module.exports = {
         }
       },
       {
-	test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        test: /\.css$/,
+        use: [{
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: "[name]_[local]_[hash:base64]",
+              sourceMap: true,
+              minimize: true
+            }
+          }
+        ]
       }
     ]
   },
-  plugins: [htmlPlugin]
+  plugins: [htmlWebpackPlugin]
 };
