b99fda5be9b176457d0eb7e0476f4d22eed63e35
shadish <shadtisch@gmail.com>
2018-08-15 23:48:56 -0600

    disable externals

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import '../css/some-style.css';
 import '../less/style.less';
+import "../images/harimau.jpg";
 
 export interface HelloProps {
   compiler: string;
@@ -12,9 +13,13 @@ export interface HelloProps {
 export class Hello extends React.Component<HelloProps, {}> {
   render() {
     return (
+      <div>
       <h1>
         Hello from {this.props.compiler} and {this.props.framework}!
       </h1>
+      <span className="tigerLabel">Tiger</span>
+      <img src="harimau.jpg"/>
+      </div>
     );
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -39,14 +39,11 @@ module.exports = {
     ]
   },
 
-  // When importing a module whose path matches one of the following, just
-  // assume a corresponding global variable exists and use that instead.
-  // This is important because it allows us to avoid bundling all of our
-  // dependencies, which allows browsers to cache those libraries between builds.
-  externals: {
-    react: "React",
-    "react-dom": "ReactDOM"
-  },
+  // Externals are ideal for library developers.
+  // externals: {
+  //   react: "React",
+  //   "react-dom": "ReactDOM"
+  // },
   plugins: [
     new HtmlWebpackPlugin({
       hash: true,
